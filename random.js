d1ba2d1877df601a5c3e1eac87984dde2e066b42
Mominul Hoque <88490184+Momin69@users.noreply.github.com>
2022-10-31 01:46:13 +0600

    first commit

---
+++ random.js
@@ -0,0 +1,24 @@
+ 
+ 
+ let Numberofwon = 0;
+let Numberoflost = 0;
+ 
+ 
+ for(let i = 1; i <=5; i++){
+    
+    let guess = parseInt(prompt("Enter a Number from 1 to 5 : "));
+
+  let randomNumber = Math.floor(Math.random()*5+1);
+
+ if (guess == randomNumber) {
+   document.write("Congratulate You are win <br/> <br/>");
+   Numberofwon++;
+}else{
+    document.write("Uff's Sorry! You have to lost.Your lucky number was ~ <br/> "+randomNumber + "<br/> <br/>");
+    Numberoflost ++;
+} 
+ }
+document.write("Total Number of won = "+Numberofwon + "<br/> <br/>");
+document.write("Totla Number of Lost = "+ Numberoflost+ "<br/> <br/>");
+
+
